b9e0426a3b8ae77f5f1bee13523ee04f848c87bc
TemitopeAgbaje <agbajeinioluwa@gmail.com>
2024-12-23 10:19:31 +0100

    fix validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strips any properties not defined in the DTO
+      forbidNonWhitelisted: true, // Throws an error for properties not defined in the DTO
+      transform: true, // Automatically transforms payloads to DTO instances
+      exceptionFactory: (errors) => {
+        // Customize the exception message
+        const formattedErrors = errors.map(
+          (error) =>
+            `${error.property} - ${Object.values(error.constraints).join(', ')}`,
+        );
+        return new BadRequestException(
+          `Validation failed: ${formattedErrors.join('; ')}`,
+        );
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT || 3100);
 }
 bootstrap();
--- src/user/create-user.dto.ts
+++ src/user/create-user.dto.ts
@@ -1,15 +1,17 @@
 import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({ message: 'Name is required' })
+  @IsString({ message: 'Name must be a string' })
   name: string;
 
-  @IsNotEmpty()
-  @IsEmail()
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsEmail({}, { message: 'Email must be a valid email address' })
   email: string;
 
-  @IsNotEmpty()
-  @IsEnum(['active', 'inactive'])
+  @IsNotEmpty({ message: 'Status is required' })
+  @IsEnum(['active', 'inactive'], {
+    message: 'Status must be either active or inactive',
+  })
   status: 'active' | 'inactive';
 }
